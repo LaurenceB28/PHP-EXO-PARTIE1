3c2584e645271f4d7a6bb91012f80f8e6b6222e8
LaurenceB28 <laurence.brd@gmail.com>
2020-11-18 15:20:31 +0100

    exophp6P1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exophp6</title>
+</head>
+<body>
+   <p><?php 
+
+  $number = 140;
+  $result = ($number + 30) / 2;
+ echo $result;
+   
+   
+   
+   
+   ?></p> 
+</body>
+</html>
